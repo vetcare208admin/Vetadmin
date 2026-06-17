f04caca2b2b4cf6567572173155e052bf5135eb7
vetcare208admin <vetcare208@gmail.com>
2026-06-17 16:02:39 +0300

    chore: remove all files

--- vetcare-backend/src/modules/files/files.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FilesService } from './files.service';
-import { PrismaService } from '../../prisma/prisma.service';
-
-@Module({
-  providers: [FilesService, PrismaService],
-  exports: [FilesService],
-})
-export class FilesModule {}
